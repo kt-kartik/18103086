96ae543c52c6cadf1d50c3bc81b0f5a2d2f49c8c
kt-kartik <62516728+kt-kartik@users.noreply.github.com>
2020-12-02 23:55:41 +0530

    Update ques2.java

--- Assignment 1/ques2.java
+++ Assignment 1/ques2.java
@@ -1,54 +1,41 @@
-import java.util.Scanner;
-public class ques2{
+import java.util.*;
+
+class vector{
+    
 	public static void main(String[] args) {
-		System.out.println("Enter the string:");
-		Scanner scan=new Scanner(System.in);
-		String p;
-		p=scan.nextLine();
-		System.out.println("Enter the size of replacement vector:");
-		int n=scan.nextInt();
-		int x=0;
-		String[] s=new String[500];
-		String[] s1=new String[n];
-		String[] s2=new String[n];
-		String temp="";
-		java.util.HashMap<String, Integer> mark = new java.util.HashMap<String, Integer>();
-		for(int i=0;i<n;i++){
-	        System.out.println("Enter word to be replaced: ");
-	        s1[i]=scan.next();
-	        s2[i]=String.valueOf(s1[i].charAt(0)); 
-	        for(int r=0;r<s1[i].length()-1; r++) {
-	        	s2[i]=s2[i]+'*';
-	        }
-	        mark.put(s1[i],i+1);}
-
-	        int k=p.length();
-
-	        for(int i=0;i<k;i++){
-	        int j=i;
-	        temp="";
-
-	        while((j<k)&&(p.charAt(j)!=' ')){
-	        temp=temp+p.charAt(j);
-	        j++;}
-
-	        if(temp!=" ")
-	        i=j;
-
-	        s[x]=temp;
-	        x++;}
-
-	        for(int i=0;i<x;i++){
-
-	        if(mark.containsKey(s[i])){
-	        int z=mark.get(s[i]);
-	        s[i]=s2[z-1];}}
-
-	        for(int i=0;i<x;i++)
-	        System.out.print(s[i]+" ");
-
-	        System.out.print("\n");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the paragraph : ");
+		String s = sc.nextLine();
+		System.out.println("Enter the number of strings : ");
+		int n = sc.nextInt();
+		System.out.println("Enter the strings : ");
 		
+		HashSet<String> vec=new HashSet<String>();
+		for(int i = 0; i < n; i++)
+		{String mm = sc.next();
+		vec.add(mm);}
 		
+		s.trim();
+		s = s + " ";
+		int i = 0;
+		String newString = "";
+		int k = 0;
+		while(s.indexOf(' ', k) != -1)
+		{
+		    k = s.indexOf(' ', k);
+		    String p = s.substring(i, k);
+		    if(vec.contains(p))
+		    {
+		        newString += p.charAt(0);
+		        for(int j = 0; j < p.length() - 1; j++)
+		        newString += "*";
+		    }
+		    else newString += p;
+		    newString += " ";
+		    k++;
+		    i = k;
+		}
+		newString.trim();
+		System.out.println(newString);
 	}
 }
